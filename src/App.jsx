6d6fb59c7e224d080ef3da4911e00b6cb2da5782
Daniel <djdani18000@gmail.com>
2026-05-20 21:09:24 +0200

    evitar warning en pagina desplegada

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const NotFound = lazy(() => import("./pages/NotFound"));
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Toaster
         position="top-right"
         toastOptions={{
